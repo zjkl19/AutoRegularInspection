0c4256c161197c4dd25afcee9694971a56aec01d
迪南 林 <eamdfan@hotmail.com>
2020-03-25 22:23:35 +0800

    upgrading unittests

--- AutoRegularInspectionTestProject/Services/SaveExcelService.cs
+++ AutoRegularInspectionTestProject/Services/SaveExcelService.cs
@@ -12,27 +12,43 @@ namespace AutoRegularInspectionTestProject.Services
     public class SaveExcelServie
     {
         [Fact]
-        public void SaveExcel()
+        public void SaveExcel_ShouldSaveVarInExcel()
         {
             //Arrange
-            var listDamageSummary = new List<DamageSummary>();
-            listDamageSummary.Add(new DamageSummary { Position = "第一跨" });
+            var listDamageSummary = new List<DamageSummary>
+            {
+                new DamageSummary { 
+                    Position = "第1跨"
+                    , Component = "伸缩缝"
+                    ,Damage="缝内沉积物阻塞"
+                    ,DamageDescription="左幅0#伸缩缝沉积物阻塞"
+                }
+            };
+            string expectedDamage = listDamageSummary[0].Damage; string acturalDamage = string.Empty;
+            string expectedDamageDescription = listDamageSummary[0].DamageDescription; string acturalDamageDescription = string.Empty;
+            string tempFileName = "temp外观检查.xlsx";
             //Act
+
+            if (File.Exists(tempFileName))
+            {
+                File.Delete(tempFileName);
+            }
+
             SaveExcelService.SaveExcel(listDamageSummary);
 
-            var file = new FileInfo("temp外观检查.xlsx");
-
-            string testString = string.Empty;
+            var file = new FileInfo(tempFileName);
 
             using (var excelPackage = new ExcelPackage(file))
             {
-                // 添加worksheet
+                // 检查"桥面系"Worksheets
                 var worksheet = excelPackage.Workbook.Worksheets["桥面系"];
-                testString = worksheet.Cells[2, 2].Value?.ToString() ?? string.Empty;
+                acturalDamage = worksheet.Cells[2, 4].Value?.ToString() ?? string.Empty;
+                acturalDamageDescription = worksheet.Cells[2, 5].Value?.ToString() ?? string.Empty;
             }
-
+            
             //Assert
-            Assert.Equal("第2跨", testString);
+            Assert.Equal(expectedDamage, acturalDamage);
+            Assert.Equal(expectedDamageDescription, acturalDamageDescription);
         }
     }
 }
