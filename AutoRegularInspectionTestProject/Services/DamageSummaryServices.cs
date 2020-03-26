578db745a1d32143b23501a9573e2c15d7adb99a
迪南 林 <eamdfan@hotmail.com>
2020-03-26 22:55:44 +0800

    add UnitTests for DamageSummaryServices

---
+++ AutoRegularInspectionTestProject/Services/DamageSummaryServices.cs
@@ -0,0 +1,55 @@
+﻿using AutoRegularInspection.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AutoRegularInspectionTestProject.Services
+{
+    public class DamageSummaryServices
+    {
+        [Fact]
+        public void SetPictureCounts_ShouldSetCorretPictureCounts()
+        {
+            //Arrange
+            var bridgeDeckListDamageSummary = new List<DamageSummary>
+            {
+                new DamageSummary {
+                    PictureNo="855,858,875"
+                }
+            };
+
+            var ds = new AutoRegularInspection.Services.DamageSummaryServices();
+
+            //Act
+
+            ds.InitListDamageSummary(bridgeDeckListDamageSummary);
+
+            //Assert
+            Assert.Equal(3, bridgeDeckListDamageSummary[0].PictureCounts);
+        }
+
+        [Fact]
+        public void SetFirstAndLastPictureBookmark_ShouldSetCorretBookmarkName()
+        {
+            //Arrange
+            var bridgeDeckListDamageSummary = new List<DamageSummary>
+            {
+                new DamageSummary {
+                    PictureNo="855,858,875"
+                }
+            };
+
+            var ds = new AutoRegularInspection.Services.DamageSummaryServices();
+
+            //Act
+
+            ds.InitListDamageSummary(bridgeDeckListDamageSummary);
+
+            //Assert
+            Assert.Equal(1000000, bridgeDeckListDamageSummary[0].FirstPictureBookmarkIndex);
+            Assert.Equal($"_Ref1000000", bridgeDeckListDamageSummary[0].FirstPictureBookmark);
+            Assert.Equal($"_Ref1000002", bridgeDeckListDamageSummary[0].LastPictureBookmark);
+        }
+    }
+}
