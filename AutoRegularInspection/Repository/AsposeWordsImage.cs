d40e05ed1a1b0e66c27e0c720d8306c8128b8801
zjkl19 <eamdfan@126.com>
2020-04-09 10:48:20 +0800

    upgrading

--- AutoRegularInspection/Repository/AsposeWordsImage.cs
+++ AutoRegularInspection/Repository/AsposeWordsImage.cs
@@ -17,34 +17,18 @@ namespace AutoRegularInspection.Repository
         /// <param name="filePath">word文件路径</param>
         /// <param name="savePath">保存文件路径</param>
         /// <returns></returns>
-        public static List<string> ExportImageFromWordFile(string filePath, string savePath = "")
+        public static IEnumerable<string> ExportImageFromWordFile(string filePath, string savePath = "")
         {
-            if (!File.Exists(filePath)) return new List<string>();
+            if (!File.Exists(filePath)) yield return string.Empty;
             if (string.IsNullOrEmpty(savePath)) savePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\Temp\\";
 
-            //文件名集合
-            List<string> list = new List<string>();
             //加载word
             Document doc = new Document(filePath);
             NodeCollection shapes = doc.GetChildNodes(NodeType.Shape, true);
             int imageIndex = 0;
-            //foreach (Shape shape in shapes)
-            //{
-            //    if (shape.HasImage)
-            //    {
-            //        string time = DateTime.Now.ToString("HHmmssfff");
-            //        //扩展名
-            //        string ex = FileFormatUtil.ImageTypeToExtension(shape.ImageData.ImageType);
-            //        //文件名
-            //        string fileName = string.Format("{0}_{1}{2}", time, imageIndex, ex);
-            //        shape.ImageData.Save(savePath + fileName);
-            //        //添加文件到集合
-            //        list.Add(fileName);
-            //        imageIndex++;
-            //    }
-            //}
+
             Shape shape;
-            for (int i=0;i<shapes.Count;i++)
+            for (int i = 0; i < shapes.Count ; i++)
             {
                 shape = shapes[i] as Shape;
                 if (shape.HasImage)
@@ -52,14 +36,13 @@ namespace AutoRegularInspection.Repository
                     //扩展名
                     string ex = FileFormatUtil.ImageTypeToExtension(shape.ImageData.ImageType);
                     //文件名
-                    string fileName = $"{imageIndex}{ex}";
+                    string fileName = $"{imageIndex + 1}{ex}";
                     shape.ImageData.Save(savePath + fileName);
-                    //添加文件到集合
-                    list.Add(fileName);
+
+                    yield return fileName;
                     imageIndex++;
                 }
             }
-            return list;
         }
 
         /// <summary>
@@ -67,8 +50,8 @@ namespace AutoRegularInspection.Repository
         /// </summary>
         /// <param name="skipBefore">跳过之前的图片数量（要测试，因为有的Shape的HasImage属性为false）</param>
         /// <param name="skipAfter">跳过之后的图片数量（要测试，因为有的Shape的HasImage属性为false）</param>
-        /// <param name="filePath"></param>
-        /// <param name="savePath"></param>
+        /// <param name="filePath">word文件路径</param>
+        /// <param name="savePath">保存文件路径</param>
         /// <returns></returns>
         public static IEnumerable<string> ExportImageFromWordFile(int skipBefore,int skipAfter,string filePath, string savePath = "")
         {
