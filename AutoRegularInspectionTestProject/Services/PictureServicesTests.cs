e30a04834eff93ffb739896ab37d5f0fe33d8b86
迪南 林 <eamdfan@hotmail.com>
2022-04-20 14:06:04 +0800

    修改验证照片的测试

--- AutoRegularInspectionTestProject/Services/PictureServicesTests.cs
+++ AutoRegularInspectionTestProject/Services/PictureServicesTests.cs
@@ -12,33 +12,38 @@ namespace AutoRegularInspectionTestProject.Services
     {
         //TODO：修改测试文件名
         [Fact]
-        public void ValidatePicturesTest()
+        public void ValidatePicturesTest_ShouldReturnCorrectInvalidPictureCounts_And_ValidationResult()
         {
             //Arrange
             var bridgeDeckListDamageSummary = new List<DamageSummary>
             {
                 new DamageSummary {
-                    PictureNo="855,858,875"
+                    PictureNo="855;858;875"
                 }
             };
             var superSpaceListDamageSummary = new List<DamageSummary>
             {
                 new DamageSummary {
-                    PictureNo="855,858,875i"
+                    PictureNo="855;858;875x"
                 }
             };
             var subSpaceListDamageSummary = new List<DamageSummary>
             {
                 new DamageSummary {
-                    PictureNo="855,858,875i"
+                    PictureNo="855;858y;875z"
                 }
             };
+
+            DamageSummaryServices.InitListDamageSummary(bridgeDeckListDamageSummary);
+            DamageSummaryServices.InitListDamageSummary(superSpaceListDamageSummary, 2_000_000, BridgePart.SuperSpace);
+            DamageSummaryServices.InitListDamageSummary(subSpaceListDamageSummary, 3_000_000, BridgePart.SubSpace);
 
             //Act
             int totalInvalidPictureCounts = PictureServices.ValidatePictures(bridgeDeckListDamageSummary, superSpaceListDamageSummary, subSpaceListDamageSummary, out List<string> bridgeDeckValidationResult, out List<string> superSpaceValidationResult, out List<string> subSpaceValidationResult);
 
             //Assert
-            Assert.Equal(2, totalInvalidPictureCounts);
+            Assert.Equal(3, totalInvalidPictureCounts);
+            Assert.Equal("875x", superSpaceValidationResult[0]);
         }
     }
 }
